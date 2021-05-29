40a108f224744797f2b522bef39899eb146f2c75
viveksemwal <Viveksemwal1997@gmail.com>
2021-05-29 09:12:54 +0530

    done added spring boot

---
+++ Backend/src/main/java/com/ecommerce/colab/ColabApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.colab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColabApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/ecommerce/colab/ColabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.colab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
